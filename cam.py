1543ce025f98e4bf70d1d0c1afb43efd8bf1148e
Vo Nhat nHuy <nhathuy@Vos-MacBook-Air-2.local>
2023-03-25 21:25:54 +0700

    done

---
+++ cam.py
@@ -0,0 +1,8 @@
+# Imports PIL module 
+from PIL import Image
+
+# open method used to open different extension image file
+im = Image.open(r"C:\Users\System-Pc\Desktop\ybear.jpg")
+
+# This method will show image in any image viewer 
+im.show() 
